a06c0415e28019ad34c6186430eeefbef63d1156
JavohirThis <javohir.this@gmail.com>
2023-07-28 16:01:45 -0400

    fifth commit

--- Admin/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from .models import AdminModel
-
-class AdminSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = AdminModel
-        fields = '__all__'
